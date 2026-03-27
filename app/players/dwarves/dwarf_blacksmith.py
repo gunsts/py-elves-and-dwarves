3e5391f2e2cc7512a511e2ec0ab4f5fc9497bfea
gunsts <137734013+gunsts@users.noreply.github.com>
2026-03-27 08:53:52 +0700

    Solution

--- app/players/dwarves/dwarf_blacksmith.py
+++ app/players/dwarves/dwarf_blacksmith.py
@@ -12,8 +12,8 @@ class DwarfBlacksmith(Dwarf):
         super().__init__(nickname, favourite_dish)
 
     def player_info(self) -> None:
-        print(f"Dwarf blacksmith {self.nickname} with skill \
-of the {self._skill_level} level")
+        return f"Dwarf blacksmith {self.nickname} with skill \
+of the {self._skill_level} level"
 
     def get_rating(self) -> None:
         return self._skill_level
--- app/players/elves/druid.py
+++ app/players/elves/druid.py
@@ -11,9 +11,9 @@ class Druid(Elf):
         self._favourite_spell = favourite_spell
         super().__init__(nickname, musical_instrument)
 
-    def player_info(self) -> None:
-        print(f"Druid {self.nickname}. {self.nickname} has a favourite spell: \
-{self._favourite_spell}")
+    def player_info(self) -> str:
+        return f"Druid {self.nickname}. {self.nickname} has a \
+favourite spell: {self._favourite_spell}"
 
     def get_rating(self) -> str:
         return len(self._favourite_spell)
